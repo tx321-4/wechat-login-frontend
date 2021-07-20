544b67a54263fd3aa849525c61831a022f3d8b81
shi_yi <shi_yidashi@163.com>
2021-07-20 11:08:35 +0800

    添加退出登陆按钮

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -2,15 +2,18 @@
   <div>
     <el-menu mode="horizontal" :router="true">
       <el-menu-item index="/">首页</el-menu-item>
-      <el-menu-item>菜单一</el-menu-item>
-      <el-menu-item>菜单二</el-menu-item>
+      <el-menu-item index="/menu1">菜单一</el-menu-item>
+      <el-menu-item index="/menu2">菜单二</el-menu-item>
       <el-menu-item index="/ucenter">个人中心</el-menu-item>
-      <el-menu-item style="float: right">
-        <template v-if="this.$store.getters.userInfo">
-          <img class="avatar" :src="this.$store.getters.userInfo.avatarUrl" alt="">
+      <el-submenu index="2" v-if="this.$store.getters.userInfo" style="float: right">
+          <template slot="title">
+           <img class="avatar" :src="this.$store.getters.userInfo.avatarUrl" alt="">
           {{this.$store.getters.userInfo.nickName}}
-        </template>
-        <div  v-else @click="handleLogin">登陆</div>
+          </template>
+          <el-menu-item @click="handleLogOut">退出登陆</el-menu-item>
+        </el-submenu> 
+      <el-menu-item v-else style="float: right">
+        <div  @click="handleLogin">登陆</div>
       </el-menu-item>
     </el-menu>
     <login :visible.sync="show"></login>
@@ -19,6 +22,8 @@
 
 <script>
 import login from '@/components/Login.vue'
+import store from '@/store'
+
 export default {
   name: 'Header', 
   data(){
@@ -32,6 +37,10 @@ export default {
   methods:{
     handleLogin(){
       this.show = true
+    },
+    handleLogOut(){
+      store.dispatch('user/logOut')
+       //this.$router.push('/')
     }
   }
 }
